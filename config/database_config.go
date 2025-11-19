479779c4d9772f37c8981e06bc13dda2446c1cb9
HarshaVardhan-reddi <harshapreddy9@gmail.com>
2025-11-19 23:34:08 +0530

    upgraded go to latest stable version & added mysql connection

--- config/database_config.go
+++ config/database_config.go
@@ -1,11 +1,19 @@
 package config
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"os"
+	"time"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/goccy/go-yaml"
+	ormsql "gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
+
+
 
 type DatabaseServer struct{
 	Host string `yaml:"host"`
@@ -21,13 +29,48 @@ type DatabaseConfig struct{
 	Production DatabaseServer
 }
 
+var db_config = DatabaseConfig{}
+
 func init(){
 	content, err := os.ReadFile("config/database.yml")
 	if err != nil {
 		log.Fatal("Database configuration",err)
 	}
-	db_config := DatabaseConfig{}
 	if err := yaml.Unmarshal(content,&db_config); err != nil{
 		log.Fatal("Datatabase configuration",err)
+	}
+}
+
+func ConfigureMySQLServer(){
+	databaseServer, err := fetchEnvBasedDatabase(&db_config, "development")
+	if err != nil {
+		log.Fatal("Database configuration", err)
+	}
+	rawMysqlConfig := mysql.Config{
+		User: databaseServer.User,
+		Passwd: databaseServer.Password,
+		DBName: databaseServer.Database,
+		Addr: fmt.Sprintf("%s:%d",databaseServer.Host, databaseServer.Port),
+		Timeout: 10 * time.Second,
+		ReadTimeout: 10 * time.Second,
+		ParseTime: true,
+    AllowNativePasswords: true,
+	}
+	MySQLConfig := ormsql.Config{
+		DSNConfig: &rawMysqlConfig,
+	}
+	db, errInDBCon := gorm.Open(ormsql.New(MySQLConfig), &gorm.Config{})
+	if(errInDBCon != nil){
+		log.Fatal("Database configuration", err)
+	}
+	log.Println("DB::",db)
+}
+
+func fetchEnvBasedDatabase(database_config *DatabaseConfig, env string) (*DatabaseServer, error) {
+	switch(env){
+	case "development":
+		return &database_config.Development, nil
+	default:
+		return &DatabaseServer{}, errors.New("database configuration not found for the requesting environment")
 	}
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,19 @@
 module store
 
-go 1.23.3
+go 1.25
 
 require github.com/gorilla/mux v1.8.1
 
-require github.com/goccy/go-yaml v1.18.0
+require (
+	github.com/go-sql-driver/mysql v1.9.3
+	github.com/goccy/go-yaml v1.18.0
+	gorm.io/driver/mysql v1.6.0
+	gorm.io/gorm v1.31.1
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/text v0.31.0 // indirect
+)
